b10e01d7a874f7dfd25fd6be41dbb4df3a92539d
Arjunprasad P M <arjunprasadpm22@gmail.com>
2023-02-13 15:24:29 +0530

    simplecoding

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("enter the first number");
+int firstNumber = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("enter the second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("the firstnumber :" + firstNumber);
+Console.WriteLine("the secondnumber :" + secondNumber);
+
+ int temporry = firstNumber;
+firstNumber= secondNumber;
+secondNumber= temporry;
+
+Console.WriteLine("the swaped number is :");
+
+Console.WriteLine( "the firstnumber :" + firstNumber);
+Console.WriteLine("the secondnumber :" + secondNumber);
